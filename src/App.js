fd557e2f8f1f1b6887a6776a2ca048d409a610f1
Sehoon In <dlstpgns0406@gmail.com>
2021-04-06 05:49:02 +0900

    feat: add base-template

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import React from 'react';
+import { createGlobalStyle } from 'styled-components';
+import TodoTemplate from './components/TodoTemplate';
 
+const GlobalStyle = createGlobalStyle`
+  body {
+    background: #e9ecef;
+  }
+`;
 
 function App() {
   return (
-      <div>dd</div>
+      <>
+        <GlobalStyle />
+        <TodoTemplate>안녕하세요</TodoTemplate>
+      </>
   );
 }
 
