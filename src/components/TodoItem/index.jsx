b787f4ab664ccb9d224585f4ae90f0a7ee1e686b
GomiLim <poil_chxn@naver.com>
2021-04-11 03:54:43 +0900

    Create: Todolist by root

--- src/components/TodoItem/index.jsx
+++ src/components/TodoItem/index.jsx
@@ -1,17 +1,85 @@
 import React from 'react';
 import styled from 'styled-components';
+import delet from '../../assets/images/delete.png';
 
 const StyleTodoItem = styled.div`
-
+  font-family: IBM Plex Sans;
+  background: #000000;
+  box-shadow: 0px 0px 1px rgba(0, 0, 0, 0.48), 0px 0px 4px rgba(0, 0, 0, 0.12);
+  border-radius: 10px;
+  margin-top: 15px;
+  color: #fff;
+  display: flex;
+  padding: 10px 15px;
+  box-sizing: border-box;
+  p{
+    margin: 0;
+  }
 `;
 
-export function TodoItem({ todos, ...props }) {
+const StyledcontentBox = styled.div`
+  width : 90%;
+`
+const StyledRoot = styled.div`
+  display: flex;
+  align-items: center;
+
+  .todo-root {
+    color: #7A7585;
+  }
+  .todo-date {
+    font-weight: 600;
+    font-size: 14px;
+    line-height: 22px;
+    color: #76A9FF;
+    margin: 0;
+    margin-left: 5px;
+  }
+`
+const StyledContent = styled.div`
+  margin-top: 5px;
+  input {
+    margin: 0;
+    display: inline-block;
+    width: 20px;
+    height: 20px;
+    background: #FF8181;
+    border: 1px solid #FF8181;
+    box-sizing: border-box;
+    border-radius: 2px;
+
+    &+p {
+      display: inline-block;
+      font-size: 20px;
+      line-height: 20px;
+      vertical-align: top;
+      color: #EEECF1;
+      margin-left: 5px;                                                                    
+    } 
+  }
+`
+const StyledClose = styled.span`
+  width: 10%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+export function TodoItem({ todos, ...rest }) {
+  //Todo : uuid적용해서 해보깅
   return (
-    <StyleTodoItem {...props}>
-      <input type="checkbox" value={todos.complete} />
-      <div>{todos.Dday}</div>
-      <div>{todos.todo}</div>
-      <span>delete</span>
-    </StyleTodoItem>
+    <StyleTodoItem {...rest}>
+      <StyledcontentBox>
+        <StyledRoot>
+          <p className="todo-root">{todos.root}</p>
+          <p className="todo-date">{todos.Dday}</p>
+        </StyledRoot>
+        <StyledContent>
+          <input type="checkbox" value={todos.complete} />
+          <p>{todos.todo}</p>
+        </StyledContent>
+      </StyledcontentBox>
+      <StyledClose><img src={delet} alt="close" /></StyledClose>
+    </StyleTodoItem >
   );
 }
--- src/container/TodoList/index.jsx
+++ src/container/TodoList/index.jsx
@@ -5,39 +5,34 @@ import { TodoItem } from "components";
 import { DummyTodoList } from 'dummy/data';
 
 const StyledTodoList = styled.div`
-  width: 512px;
-  height: 768px;
-
-  position: relative; 
-  background: #2A2731;
-  border-radius: 16px; 
-  box-shadow: 0 0 8px 0 rgba(0, 0, 0, 0.04);
-
-  margin: 0 auto;
-
-  margin-top: 96px;
-  margin-bottom: 32px;
-  display: flex;
-  flex-direction: column;
+  padding: 25px 0;
+  .emptyTxt {
+    text-align: center;
+    color: #7A7585;
+    font-size: 20px;
+    font-weight: 600;
+  }
 `;
 
+
 function TodoList({ changeTabMenu }) {
-  console.log(DummyTodoList.length > 0)
+  const todoLength = DummyTodoList.filter(todo => {
+    return todo.id === changeTabMenu;
+  }).length;
+
   return (
     <StyledTodoList>
-      {DummyTodoList.length > 0 ?
-        changeTabMenu === 1 ?
-          DummyTodoList.map((todo, index) => {
-            return <TodoItem key={index} todos={todo} />
-          })
-          : DummyTodoList.filter(todo => {
-            return todo.id === changeTabMenu;
-          }).map((todo, index) => {
-            return <TodoItem key={index} todos={todo} />
-          })
-        :
-        <div>목록을 추가해 주세요.</div>
+      { DummyTodoList && changeTabMenu === 1 ?
+        DummyTodoList.map((todo, index) => {
+          return <TodoItem key={index} todos={todo} />
+        })
+        : DummyTodoList && DummyTodoList.filter(todo => {
+          return todo.id === changeTabMenu;
+        }).map((todo, index) => {
+          return <TodoItem key={index} todos={todo} />
+        })
       }
+      {todoLength === 0 && <p className="emptyTxt">리스트가 비어있습니다.</p>}
     </StyledTodoList >);
 }
 
